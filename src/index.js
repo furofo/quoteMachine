9896dd80cf9be4c3ec318db4bc13d9ed7c52ac10
furofo <chriscellphonerepair@gmail.com>
2020-05-09 21:28:58 -0400

    added initial conatiners for quote machine

--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-console.log('this is a test');
+class QuoteContainer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div id = "quoteBox">
+
+
+
+            <div id = "text">
+                <p>This should be text </p>
+            </div>
+
+
+            <div id = "author">
+
+
+            </div>
+
+
+            <div id = "new-quote">
+
+            </div>
+
+
+            <div id = "tweet-quote">
+
+            </div>
+
+
+
+            </div>
+        )
+    }
+}
+
+ReactDom.render(<QuoteContainer />, document.getElementById('root'));
