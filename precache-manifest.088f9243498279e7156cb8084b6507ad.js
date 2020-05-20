bce4137fe59035e3a552a60963735382789eec1b
furofo <chriscellphonerepair@gmail.com>
2020-05-19 21:39:01 -0400

    Updates

---
+++ precache-manifest.088f9243498279e7156cb8084b6507ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20188264b4ed6fb9aff457f332ca94e",
+    "url": "/quoteMachine/index.html"
+  },
+  {
+    "revision": "3d46202c20c6d6d22778",
+    "url": "/quoteMachine/static/css/main.6f62a03a.chunk.css"
+  },
+  {
+    "revision": "a8e0b4da525b5a22036a",
+    "url": "/quoteMachine/static/js/2.80ec5d9b.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/quoteMachine/static/js/2.80ec5d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d46202c20c6d6d22778",
+    "url": "/quoteMachine/static/js/main.c2cf6051.chunk.js"
+  },
+  {
+    "revision": "8c0f7ee9a3ec75296c16",
+    "url": "/quoteMachine/static/js/runtime-main.a2a37ff5.js"
+  }
+]);
